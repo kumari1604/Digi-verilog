eadf081f9e64638cbf0bb2c0d349c1cdc29fac86
Ratnakumari Gedela <gedelaratnakumari@gmail.com>
2025-07-05 13:03:18 +0530

    Update halfadder.v

--- halfadder.v
+++ halfadder.v
@@ -14,26 +14,8 @@ module tb_halfadder();
 reg a,b;
 wire sum,carry;
 halfadder uut(.a(a),.b(b),.sum(sum),.carry(carry));
-
-initial begin
-  module tb_halfadder();
-reg a,b;
-wire sum,carry;
-halfadder uut(.a(a),.b(b),.sum(sum),.carry(carry));
-
 initial begin
 $monitor(" time=%0t,a=%d,b=%d,sum=%d,carry=%d",$time,a,b,sum,carry);
-a=0;b=0;
-#10;
-a=0;b=1;
-#10;
-a=1;b=0;
-#10;
-a=1;b=1;
-#10;
-$finish;
-end
-endmodule
 a=0;b=0;
 #10;
 a=0;b=1;
